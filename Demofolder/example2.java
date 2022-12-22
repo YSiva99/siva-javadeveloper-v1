7471f62d439fe016fe50d2d8962ffbd951ce2806
siva <siva@gmail.com>
2022-12-22 16:17:35 +0530

    modified the example2 in demofolder

--- Demofolder/example2.java
+++ Demofolder/example2.java
@@ -1,5 +1,5 @@
 class Sample{
     public static void main( String[] args){
-        System.out.println( "add the new Branch in git from devteam1");
+        System.out.println( "Added the new Branch in git from devteam1");
     }
 }
