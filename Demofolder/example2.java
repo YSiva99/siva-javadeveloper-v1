01b74fc2c0c418a3feb19f7153436d4d3bdc1cc3
siva <siva@gmail.com>
2022-12-23 10:31:29 +0530

    modified the code

--- Demofolder/example2.java
+++ Demofolder/example2.java
@@ -1,6 +1,6 @@
 class Sample{
     public static void main( String[] args){
         System.out.println( "Added the new Branch in git from devteam1");
-         System.out.println( "pull the brach devteam1 changes to loacl repository");
+        
     }
 }
